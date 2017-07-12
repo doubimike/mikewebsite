630a46e2d7e8523346bcea6ca165c32359e98055
mike.zhang <mike.zhang@yeamoney.cn>
2017-07-12 11:32:12 +0800

    add collections

--- client/src/components/Hello.vue
+++ client/src/components/Hello.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>我的网站</h2>
+    <h2>我的作品</h2>
     <ul>
-      <li><a href="https://mikecollections.herokuapp.com/" target="_blank">我的作品集</a></li>
+      <li><a href="https://mikecollections.herokuapp.com/" target="_blank">我的博客</a></li>
       <li><a href="http://writelrc.qdgls.cn/#!/main" target="_blank">写歌词</a></li>
       <li><a href="http://qdgls.cn/" target="_blank">前端鼓励狮</a></li>
+    </ul>
+    <h2>我的临摹</h2>
+    <ul>
+      <li><a href="http://www.doubimike.qdgls.cn/collections/brick_game" target="_blank">Canvas砖块游戏</a></li>
+    </ul>
+    <h2>我的外包</h2>
+    <ul>
+      <li><a href="http://www.shengyefinance.com/" target="_blank">盛业资本</a></li>
+      <li><a href="http://www.ergedd.com/" target="_blank">儿歌点点</a></li>
+      <li><a href="http://www.lpz56.com/" target="_blank">蓝胖子物流</a></li>
     </ul>
   </div>
 </template>
