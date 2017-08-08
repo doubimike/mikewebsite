a304e0cfe28751c79a5bad4768a5a9cede71258d
zhanghang <mike.zhang@yeamoney.cn>
2017-08-08 20:56:48 +0800

    gzip set

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const app = new Koa()
+const compress = require('koa-compress')
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
@@ -14,26 +15,34 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
+app.use(compress({
+    filter: function(content_type) {
+        return /text/i.test(content_type)
+    },
+    threshold: 2048,
+    flush: require('zlib').Z_SYNC_FLUSH
+}))
+
 app.use(require('koa-static')(__dirname + '/client/dist/'))
 app.use(require('koa-static')(__dirname + '/client/other_collections/animation_cv/es6/build/'))
 app.use(require('koa-static')(__dirname + '/client/other_collections/canvas_particle_animator_new/'))
 app.use(require('koa-static')(__dirname + '/client/other_collections/make_a_movie_by_javascript_new/'))
 
 app.use(views(__dirname + '/client', {
-  extension: 'html'
+    extension: 'html'
 }))
 
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+app.use(async(ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routes
